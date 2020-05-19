8095d184ee6896dcdb21ad1c2264fd6f2a74032c
johan herrera <johan_16892@hotmail.com>
2020-05-18 20:49:23 -0500

    conexion a base remota

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -4,9 +4,9 @@ class Conexion{
 
 	static public function conectar(){
 
-		$link = new PDO("mysql:host=localhost;dbname=pos",
-			            "root",
-			            "");
+		$link = new PDO("mysql:host=bsfjtyozryez85iy5bpq-mysql.services.clever-cloud.com;dbname=bsfjtyozryez85iy5bpq",
+			            "u7ahjhw2ixs0hjqi",
+			            "1lgyjYrFTgIODcTXCW2P");
 
 		$link->exec("set names utf8");
 
